2dc9cb4b0622638e18bac163e62a360525c718ba
Ethan Snow <bombitmanbomb@gmail.com>
2021-06-02 02:18:56 -0400

    Update MathX.ts

--- src/MathX.ts
+++ src/MathX.ts
@@ -2,9 +2,16 @@ export class MathX {
 	public static Clamp(val: number, min: number, max: number): number {
 		// NUMBER
 		if (val < min) return min;
-		return val >= max ? max - 1 : val;
+		return val >= max ? max : val;
 	}
 	public static Sqrt(d: number): number {
 		return Math.sqrt(d);
+	}
+	public static FloatRepeat(val: number, length: number): number {
+		return val - Math.floor(val / length) * length;
+	}
+	public static Clamp01(val: number): number {
+		if (val < 0) return 1;
+		return val > 1 ? 1 : val;
 	}
 }
