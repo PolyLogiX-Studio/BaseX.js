99ea4fd9c458dc494ab174a5ffc457934f270b95
Ethan Snow <bombitmanbomb@gmail.com>
2021-06-03 22:34:10 -0400

    Update MeshX.ts

--- src/MeshX.ts
+++ src/MeshX.ts
@@ -1,4 +1,4 @@
-import { Dictionary, List } from "@bombitmanbomb/utils";
+import { Dictionary, List, Out } from "@bombitmanbomb/utils";
 import { float3 } from "./float3";
 import { float4 } from "./float4";
 import { color } from "./color";
@@ -6,6 +6,12 @@ import { float2 } from "./float2";
 import { Submesh } from "./Submesh";
 import { TriangleCollection } from "./TriangleCollection";
 import { Vertex } from "./Vertex";
+import type { VerticesPerFaceHandler } from "./util/Mikktspace/VerticesPerFaceHandler";
+import type { VetrexPositionHandler } from "./util/Mikktspace/VetrexPositionHandler";
+import type { VertexNormalHandler } from "./util/Mikktspace/VertexNormalHandler";
+import type { VertexUVHandler } from "./util/Mikktspace/VertexUVHandler";
+import type { BasicTangentHandler } from "./util/Mikktspace/BasicTangentHandler";
+import { MikkGenerator } from "./util/Mikktspace/MikkGenerator";
 export class MeshX {
 	public MESHX_BINARY_VERSION = 6;
 	public MAGIC_STRING = "MeshX";
@@ -40,7 +46,139 @@ export class MeshX {
 			triangleCollection != null
 				? triangleCollection.Count
 				: this.TotalFaceCount;
+		let verticesPerFaceHandler: VerticesPerFaceHandler;
+		let vertexPositionHandler: VetrexPositionHandler;
+		let vertexNormalHandler: VertexNormalHandler;
+		let vertexUvHandler: VertexUVHandler;
+		let basicTangentHandler: BasicTangentHandler;
+		if (triangles == null) {
+			verticesPerFaceHandler = ((face: number) => {
+				const submeshForFace: Submesh = this.GetSubmeshForFace(face);
+				return submeshForFace != null ? 0 : submeshForFace.IndiciesPerElement;
+			}).bind(this);
+			vertexPositionHandler = ((
+				face: number,
+				vert: number,
+				x: Out<number>,
+				y: Out<number>,
+				z: Out<number>
+			) => {
+				const float: float3 =
+					positions[
+						this.GetTriangleByFaceIndex(face).GetVertexIndexUnsafe(vert)
+					];
+				x.Out = float.x;
+				y.Out = float.y;
+				z.Out = float.z;
+			}).bind(this);
+			vertexNormalHandler = ((
+				face: number,
+				vert: number,
+				x: Out<number>,
+				y: Out<number>,
+				z: Out<number>
+			) => {
+				const float: float3 =
+					normals[this.GetTriangleByFaceIndex(face).GetVertexIndexUnsafe(vert)];
+				x.Out = float.x;
+				y.Out = float.y;
+				z.Out = float.z;
+			}).bind(this);
+			vertexUvHandler = ((
+				face: number,
+				vert: number,
+				x: Out<number>,
+				y: Out<number>
+			) => {
+				const float: float2 =
+					uvs[this.GetTriangleByFaceIndex(face).GetVertexIndexUnsafe(vert)];
+				x.Out = float.x;
+				y.Out = float.y;
+			}).bind(this);
+			basicTangentHandler = ((
+				face: number,
+				vert: number,
+				x: number,
+				y: number,
+				z: number,
+				sign: number
+			) =>
+				(tangents[
+					this.GetTriangleByFaceIndex(face).GetVertexIndexUnsafe(vert)
+				] = new float4(x, y, z, sign))).bind(this);
+		} else {
+			verticesPerFaceHandler = (face) => 3;
+			vertexPositionHandler = ((
+				face: number,
+				vert: number,
+				x: Out<number>,
+				y: Out<number>,
+				z: Out<number>
+			) => {
+				const float: float3 =
+					positions[triangles.THIS_GET(face).GetVertexIndexUnsafe(vert)];
+				x.Out = float.x;
+				y.Out = float.y;
+				z.Out = float.z;
+			}).bind(this);
+			vertexNormalHandler = ((
+				face: number,
+				vert: number,
+				x: Out<number>,
+				y: Out<number>,
+				z: Out<number>
+			) => {
+				const float: float3 =
+					normals[triangles.THIS_GET(face).GetVertexIndexUnsafe(vert)];
+				x.Out = float.x;
+				y.Out = float.y;
+				z.Out = float.z;
+			}).bind(this);
+			vertexUvHandler = ((
+				face: number,
+				vert: number,
+				x: Out<number>,
+				y: Out<number>
+			) => {
+				const float: float2 =
+					uvs[triangles.THIS_GET(face).GetVertexIndexUnsafe(vert)];
+				x.Out = float.x;
+				y.Out = float.y;
+			}).bind(this);
+			basicTangentHandler = ((
+				face: number,
+				vert: number,
+				x: number,
+				y: number,
+				z: number,
+				sign: number
+			) =>
+				(tangents[
+					triangles.THIS_GET(face).GetVertexIndexUnsafe(vert)
+				] = new float4(x, y, z, sign))).bind(this);
+		}
+		const getVerticesPerFace: VerticesPerFaceHandler = verticesPerFaceHandler;
+		const getPosition: VetrexPositionHandler = vertexPositionHandler;
+		const getNormal: VertexNormalHandler = vertexNormalHandler;
+		const getUV: VertexUVHandler = vertexUvHandler;
+		const setTangentBasic: BasicTangentHandler = basicTangentHandler;
+		return MikkGenerator.GenerateTangentSpace(
+			faceCount,
+			getVerticesPerFace,
+			getPosition,
+			getNormal,
+			getUV,
+			setTangentBasic
+		);
 	}
+
+	public GetVertices<T>(array:T[], convert:(float3:float3)=>T) {
+		if (array.length < this.VertexCount)
+			throw new RangeError("array.length");
+			for (let index = 0; index < this.VertexCount; index++)
+				array[index] = convert(this.positions[index])
+	}
+	
 }
 export class UV_Array {
 	public uv_2D!: float2[];
