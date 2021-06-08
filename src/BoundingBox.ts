a6ce689c4f6e6054606c795d27f50bdd5f14b701
Ethan Snow <bombitmanbomb@gmail.com>
2021-06-08 05:44:00 -0400

    Update BoundingBox.ts

--- src/BoundingBox.ts
+++ src/BoundingBox.ts
@@ -1,9 +1,9 @@
-import { float3 } from './float3';
+import { float3 } from "./float3";
 export class BoundingBox {
-  public VERTEX_POINT_COUNT = 8
-  public min:float3
+	public VERTEX_POINT_COUNT = 8;
+	public min: float3;
 }
 export interface BoundingBoxJSON {
-  min:number
-  max:number
+	min: number;
+	max: number;
 }
