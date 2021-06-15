d9be7329f63a87b7d0dac92d0ce6e40d3be00849
Ethan Snow <bombitmanbomb@gmail.com>
2021-06-15 16:29:22 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 "use strict";
-export {color} from "./color"
-export {color32} from "./color32"
-export {float2} from "./float2"
-export {float3} from "./float3"
-export {float4} from "./float4"
-export {bool2} from "./bool2"
-export {bool3} from "./bool3"
-export {bool4} from "./bool4"
+export { color } from "./color";
+export { color32 } from "./color32";
+export { float2 } from "./float2";
+export { float3 } from "./float3";
+export { float4 } from "./float4";
+export { bool2 } from "./bool2";
+export { bool3 } from "./bool3";
+export { bool4 } from "./bool4";
 
-
-export {MathX} from "./MathX"
-export {PrimitiveCircularBuffer} from "./PrimitiveCircularBuffer"
-export {PrimitivesUtility} from "./PrimitivesUtility"
+export { MathX } from "./MathX";
+export { PrimitiveCircularBuffer } from "./PrimitiveCircularBuffer";
+export { PrimitivesUtility } from "./PrimitivesUtility";
